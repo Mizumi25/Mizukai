4518f0d63b1fc7c810ca58aef1bd63aaca712fa2
Mizumi25 <Mizumikaitoart@gmail.com>
2025-06-16 06:44:32 +0800

    second fix

--- src/app/works/gameDev/page.tsx
+++ src/app/works/gameDev/page.tsx
@@ -9,108 +9,112 @@ import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 
 const GameAppDev: React.FC = () => {
   interface App {
-      id: number;
-      img: string;
-      title: string;
-      genre: string;
-      description: string;
+  id: number;
+  img: string;
+  title: string;
+  genre: string;
+  description: string;
+}
+
+const [apps, setApps] = useState<App[]>([]);
+
+useEffect(() => {
+  const newApps: App[] = AppsData.map((app, index) => ({
+    id: app.id ?? index, // fallback if app.id doesn't exist
+    img: app.img,
+    title: app.title,
+    genre: app.genre,
+    description: app.description
+  }));
+  setApps(newApps);
+}, []);
+
+useEffect(() => {
+  gsap.registerPlugin(ScrollTrigger);
+
+  ScrollTrigger.create({
+    trigger: ".section5",
+    start: "top top",
+    endTrigger: ".sectionWhitespace",
+    end: "bottom top",
+    pin: true,
+    pinSpacing: false,
+  });
+
+  ScrollTrigger.create({
+    trigger: ".section3",
+    start: "top top",
+    endTrigger: ".sectionWhitespace",
+    end: "bottom center",
+    pin: true,
+    pinSpacing: false,
+  });
+
+  ScrollTrigger.create({
+    trigger: ".section5",
+    start: "top top",
+    endTrigger: ".section3",
+    end: "bottom bottom",
+    onUpdate: (self) => {
+      const rotation = self.progress * 360;
+      gsap.to(".revealer", { rotation });
     }
-    
-    const [apps, setApps] = useState<App[]>([]);
-    
-    // Your useEffect should now work without errors
-    useEffect(() => {
-     const newApps = AppsData.map((app) => ({
-        img: app.img,
-        title: app.title,
-        genre: app.genre,
-        description: app.description
-      }));
-      setApps(newApps);
-    }, []);
-  
-  useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger)
-      ScrollTrigger.create({
-        trigger: ".section5",
-        start: "top top",
-        endTrigger: ".sectionWhitespace",
-        end: "bottom top",
-        pin: true,
-        pinSpacing: false,
+  });
+
+  ScrollTrigger.create({
+    trigger: ".section5",
+    start: "top top",
+    endTrigger: ".section3",
+    end: "bottom bottom",
+    onUpdate: (self) => {
+      const progress = self.progress;
+      const clipPath = `polygon(
+        ${45 - 45 * progress}% ${0 + 0 * progress}%,
+        ${55 + 45 * progress}% ${0 + 0 * progress}%,
+        ${55 + 45 * progress}% ${100 - 0 * progress}%,
+        ${45 - 45 * progress}% ${100 - 0 * progress}%
+      )`;
+      gsap.to(".revealer1, .revealer2", {
+        clipPath: clipPath,
+        ease: "none",
+        duration: 0,
       });
-      ScrollTrigger.create({
-        trigger: ".section3",
-        start: "top top",
-        endTrigger: ".sectionWhitespace",
-        end: "bottom center",
-        pin: true,
-        pinSpacing: false,
+    }
+  });
+
+  ScrollTrigger.create({
+    trigger: ".section3",
+    start: "top top",
+    end: "bottom 50%",
+    scrub: 1,
+    onUpdate: (self) => {
+      const progress = self.progress;
+      const left = 35 + 15 * progress;
+      gsap.to(".revealer", {
+        left: `${left}%`,
+        ease: "none",
+        duration: 0,
       });
-      ScrollTrigger.create({
-        trigger: ".section5",
-        start: "top top",
-        endTrigger: ".section3",
-        end: "bottom bottom",
-        onUpdate: (self) => {
-          const rotation = self.progress * 360
-          gsap.to(".revealer", { rotation })
-        }
+    }
+  });
+
+  ScrollTrigger.create({
+    trigger: ".sectionWhitespace",
+    start: "top 50%",
+    end: "bottom bottom",
+    scrub: 1,
+    onUpdate: (self) => {
+      const progress = self.progress;
+      const scale = 1 + 30 * progress;
+      gsap.to(".revealer", {
+        scale: scale,
+        ease: "none",
+        duration: 0,
       });
-      ScrollTrigger.create({
-        trigger: ".section5",
-        start: "top top",
-        endTrigger: ".section3",
-        end: "bottom bottom",
-        onUpdate: (self) => {
-          const progress = self.progress
-          const clipPath = `polygon(
-            ${45 - 45 * progress}% ${0 + 0 * progress}%,
-            ${55 + 45 * progress}% ${0 + 0 * progress}%,
-            ${55 + 45 * progress}% ${100 - 0 * progress}%,
-            ${45 - 45 * progress}% ${100 - 0 * progress}%
-            )`;
-          gsap.to(".revealer1, .revealer2", { 
-            clipPath: clipPath,
-            ease: "none",
-            duration: 0,
-          })
-        }
-      });
-      ScrollTrigger.create({
-        trigger: ".section3",
-        start: "top top",
-        end: "bottom 50%",
-        scrub: 1,
-        onUpdate: (self) => {
-          const progress = self.progress
-          const left = 35 + 15 * progress
-          gsap.to(".revealer", { 
-            left: `${left}%`,
-            ease: "none",
-            duration: 0,
-          })
-        }
-      });
-      ScrollTrigger.create({
-        trigger: ".sectionWhitespace",
-        start: "top 50%",
-        end: "bottom bottom",
-        scrub: 1,
-        onUpdate: (self) => {
-          const progress = self.progress
-          const scale = 1 + 30 * progress
-          gsap.to(".revealer", { 
-            scale: scale,
-            ease: "none",
-            duration: 0,
-          })
-        }
-      });
-    
-    
-    
-  }, []);
+    }
+  });
+}, []);
+
   
   
   
